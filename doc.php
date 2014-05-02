fe5eb7d173dbdfb4368ce137b3029b2679a38b69
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2014-05-02 16:31:14 -0700

    fix doc.php for SQL Injection vulnerable point.

--- doc.php
+++ doc.php
@@ -4,11 +4,11 @@ include "config.php";
 
 $task_id = $_GET['id'];
 
-$query = 'select `name_short`, `see` from `task` where `task_id` = ?';
+$query = 'select `name`, `name_short`, `see` from `task` where `task_id` = ?';
 $sql->prepare($query);
 $sql->bind_param('i',$task_id);
 $sql->execute();
-$sql->bind_result($name_short, $visible);
+$sql->bind_result($name, $name_short, $visible);
 
 if($sql->fetch()) {
 	$path = 'doc/' . $name_short . '.pdf';
@@ -21,10 +21,13 @@ if($sql->fetch()) {
 	    ob_clean();
 	    flush();
 	    readfile($path);
-	    $sqlx = "select * from `task` where `task_id` = ".$task_id;
-		$result = mysql_query($sqlx);
-		$task = mysql_fetch_object($result);
-	    echo "<title>".$task->name." ".($name_short)."</title>";
+
+	    // Vulnerable to SQL Injection!
+	    // Because $task_id is from $_GET['id'] which is given by the user.
+	  	// $sqlx = "select * from `task` where `task_id` = ".$task_id;
+			// $result = mysql_query($sqlx);
+			// $task = mysql_fetch_object($result);
+	    echo "<title>".$name." ".($name_short)."</title>";
 	    exit;
 	  } else {
 	  	echo 'this file is not visible by now.';
