634f88d12458f1ad4caeb42acfd3eb8cf188400c
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2014-10-15 13:16:50 +0700

    no message

--- admin/rejudge.php
+++ admin/rejudge.php
@@ -18,10 +18,13 @@ if (isset($_SESSION['user_id'])) {
 
           // get the user_id via username
           $user_id = null;
-          $query = "select `user_id` from `user` where `user` = {$user} limit 1";
+          $query = "select `user_id` from `user` where `user` = '{$user}' limit 1";
           $result = mysql_query($query) or die(mysql_error());
           $user_info = mysql_fetch_array($result);
           $user_id = $user_info['user_id'];
+          if (!$user_id) {
+            continue;
+          }
 
           //enqueue
           $date = new DateTime();
@@ -34,7 +37,7 @@ if (isset($_SESSION['user_id'])) {
             `time` ,
             `file`
             )
-            VALUES (NULL , {$user_id}, {$task_id}, {$now}, {$entry})";
+            VALUES (NULL , {$user_id}, {$task_id}, {$now}, '{$entry}')";
           $result = mysql_query($query) or die(mysql_error());
 
           echo "added {$entry} to rejudge.<br>";
