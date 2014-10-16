c72edd878eedcc1379873eca690d6c1dc81881d0
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2014-10-16 17:59:51 +0700

    rejudger now will decide which code either .c or .cpp is newer to be judged

--- admin/rejudge.php
+++ admin/rejudge.php
@@ -6,6 +6,8 @@ if (isset($_SESSION['user_id'])) {
 
   $dir = '../judge/upload';
 
+  $list = array();
+  // make the list
   if ($handle = opendir($dir)) {
     // search in the directory
     while (($entry = readdir($handle)) !== false) {
@@ -16,42 +18,52 @@ if (isset($_SESSION['user_id'])) {
           // get the username
           $user = explode('.', $chunks[1])[0];
 
-          // get the user_id via username
-          $user_id = null;
-          $query = "select `user_id` from `user` where `user` = '{$user}' limit 1";
-          $result = mysql_query($query) or die(mysql_error());
-          $user_info = mysql_fetch_array($result);
-          $user_id = $user_info['user_id'];
-          if (!$user_id) {
-            continue;
-          }
-
-          //remove pass
-          $query = "delete from `pass` where `task_id` = {$task_id}";
-          $result = mysql_query($query) or die(mysql_error());
-
-          //remove bests
-          $query = "delete from `best` where `task_id` = {$task_id}";
-          $result = mysql_query($query) or die(mysql_error());
-
-          //enqueue
-          $date = new DateTime();
-          $now = $date->getTimestamp();
-          $query = "
-            INSERT INTO `queue` (
-            `queue_id` ,
-            `user_id` ,
-            `task_id` ,
-            `time` ,
-            `file`
-            )
-            VALUES (NULL , {$user_id}, {$task_id}, {$now}, '{$entry}')";
-          $result = mysql_query($query) or die(mysql_error());
-
-          echo "added {$entry} to rejudge.<br>";
+          // select only the latest file (either .c, .cpp)
+          $mod_time = filemtime($dir . '/' . $entry);
+          echo $mod_time . "<br>";
+          if (!isset($list[$user])
+            || (isset($list[$user]) && $list[$user]['mod_time'] < $mod_time))
+            $list[$user] = array('file' => $entry, 'mod_time' => $mod_time);
         }
       }
     }
+  }
+  // get from list
+  foreach ($list as $user => $each) {
+    $entry = $each['file'];
+    // get the user_id via username
+    $user_id = null;
+    $query = "select `user_id` from `user` where `user` = '{$user}' limit 1";
+    $result = mysql_query($query) or die(mysql_error());
+    $user_info = mysql_fetch_array($result);
+    $user_id = $user_info['user_id'];
+    if (!$user_id) {
+      continue;
+    }
+
+    //remove pass
+    $query = "delete from `pass` where `task_id` = {$task_id}";
+    $result = mysql_query($query) or die(mysql_error());
+
+    //remove bests
+    $query = "delete from `best` where `task_id` = {$task_id}";
+    $result = mysql_query($query) or die(mysql_error());
+
+    //enqueue
+    $date = new DateTime();
+    $now = $date->getTimestamp();
+    $query = "
+      INSERT INTO `queue` (
+      `queue_id` ,
+      `user_id` ,
+      `task_id` ,
+      `time` ,
+      `file`
+      )
+      VALUES (NULL , {$user_id}, {$task_id}, {$now}, '{$entry}')";
+    $result = mysql_query($query) or die(mysql_error());
+
+    echo "added {$entry} to rejudge.<br>";
   }
 } else {
   die("you are neither admin nor logged in.");
