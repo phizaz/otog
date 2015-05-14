701e80c749cd8a7876a2e995265e9116dc7b4d0b
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2015-05-14 23:59:07 +0700

    no message

--- doc.php
+++ doc.php
@@ -14,11 +14,14 @@ if($sql->fetch()) {
 	$path = 'doc/' . $name_short . '.pdf';
 	if(file_exists($path)){
 		if($visible or isAdmin()) {
-			header("Pragma: public");
-			header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-	    header('Content-Type: application/pdf');
-	    header('Content-Disposition: inline; filename="'. $name_short . '.pdf"');
-	    header('Content-Length: ' . filesize($path));
+			header('Content-type: application/pdf');
+		  header('Content-Disposition: inline; filename="' . $name_short . '"');
+		  header('Content-Transfer-Encoding: binary');
+		  header('Accept-Ranges: bytes');
+		  // old header
+	    // header('Content-Type: application/pdf');
+	    // header('Content-Disposition: inline; filename="'. $name_short . '.pdf"');
+	    // header('Content-Length: ' . filesize($path));
 	    // ob_clean();
 	    // flush();
 	    @readfile($path);
